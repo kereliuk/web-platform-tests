c8d3b34971e6bdc4e8e86abc1b115d62e62c5737
Jonathon Kereliuk <kereliuk@chromium.org>
2018-02-27 18:58:21 -0500

    try out edge driver

--- tools/wptrunner/wptrunner/executors/executoredge.py
+++ tools/wptrunner/wptrunner/executors/executoredge.py
@@ -1,10 +1,16 @@
 from ..webdriver_server import EdgeDriverServer
 from .base import WdspecExecutor, WebDriverProtocol
-
+from .executorwebdriver import WebDriverTestharnessExecutor, WebDriverExecutorProtocol
 
 class EdgeDriverProtocol(WebDriverProtocol):
     server_cls = EdgeDriverServer
 
 
 class EdgeDriverWdspecExecutor(WdspecExecutor):
-    protocol_cls = EdgeDriverProtocol
+    protocol_cls = EdgeDriverProtocol 
+
+class EdgeDriverExecutorProtocol(WebDriverExecutorProtocol):
+    server_cls = EdgeDriverServer
+
+class EdgeDriverTestHarnessExecutor(WebDriverTestharnessExecutor):
+    protocol_cls = EdgeDriverExecutorProtocol
